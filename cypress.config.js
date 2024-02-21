79e7eb79dc03df3e9135bb4a93df93e6d805f19e
darkhorse <darkhorse109707@gmail.com>
2024-02-21 15:25:53 +0100

    feat: add dependabot (#11)
    * feat: add dependabot
    
    * fix: dependencies added to auto-merge.yml file
    
    * fix: /ping removed
    
    * fix: ruby added to auto-merge.yml file
    
    * fix: extract port from base url if api url is not provided
    
    * fix: localhost added as host
    
    * fix: wait-on timout loaded from variables and listen to port instead of url
    
    * fix: cypress updated to 12.17.1
    
    * fix: cypress config updated
    
    * fix: step added to run test if api url is set
    
    * fix: write permission added to the workflow and automerge will be triggered on patch update
    
    * fix: automerge will be triggered on all update
    
    ---------
    
    Co-authored-by: Ehsan Mirzarazi <ehsanmirzarazi@ehsans-mbp.home>
    Co-authored-by: Ehsan Mirzarazi <ehsanmirzarazi@Ehsans-MBP.lan>

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+  },
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+  },
+})
